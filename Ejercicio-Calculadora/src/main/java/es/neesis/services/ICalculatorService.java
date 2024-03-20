7b78a6fa59cf635991f74aafbf1e4caec09dc262
davidHerreroBabel <161601498+davidHerreroBabel@users.noreply.github.com>
2024-03-20 12:34:35 +0100

    Delete Ejercicio-Calculadora directory

--- Ejercicio-Calculadora/src/main/java/es/neesis/services/ICalculatorService.java
+++
@@ -1,9 +0,0 @@
-package es.neesis.services;
-
-public interface ICalculatorService {
-
-    public Double calculateResult(double value1, double value2, String operation);
-
-    public Boolean isOperationExit(String operation);
-
-}
